2bb8014b23d8456b27eaecc37bb710da7c98f9b7
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-05-21 01:07:42 +0100

    sidebar styling

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,23 @@
 import './sidebar.scss';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className='sidebar'>
+      <div className='top'>
+        <div className='logo'>Smart Store</div>
+      </div>
+      <hr />
+      <div className='center'>
+        <ul>
+          <li>Dashboard</li>
+          <li>Dashboard</li>
+          <li>Dashboard</li>
+          <li>Dashboard</li>
+        </ul>
+      </div>
+      <div className='bottom'>color options</div>
+    </div>
+  );
 };
 
 export default Sidebar;
