04f19bfd4263d374461bd08f94ab454cf81bda69
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-05-23 14:10:57 +0100

    navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,44 @@
 import './navbar.scss';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+// import { DarkModeContext } from '../../context/darkModeContext';
+import { useContext } from 'react';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='wrapper'>
-        <div className='search'></div>
-        <input type='text' placeholder='Search...' />
-        <SearchOutlinedIcon />
-      </div>
-      <div className='items'>
-        <div className='item'></div>
+        <div className='search'>
+          <input type='text' placeholder='Search...' />
+          <SearchOutlinedIcon />{' '}
+        </div>
+
+        <div className='items'>
+          <div className='item'>
+            <LanguageOutlinedIcon />
+            English
+          </div>
+          <div className='item'>
+            <DarkModeOutlinedIcon />
+          </div>
+          <div className='item'>
+            <FullscreenExitOutlinedIcon />
+          </div>
+          <div className='item'>
+            <NotificationsNoneOutlinedIcon />
+          </div>
+          <div className='item'>
+            <ChatBubbleOutlineOutlinedIcon />
+          </div>
+          <div className='item'>
+            <ListOutlinedIcon />
+          </div>
+        </div>
       </div>
     </div>
   );
